1049fcf5f383fbcd489c3e09c03ab942d61e487a
Vadim Akulik <lodkaby@gmail.com>
2021-03-16 09:38:32 +0300

    fix-1

--- main.js
+++ main.js
@@ -101,3 +101,73 @@ const PRODUCT_TYPE = {
       type: PRODUCT_TYPE.VEGETABLE,
     },
   ];
+
+  //Домашка 16
+ 
+  /*function filterProducts(products, productType) {
+    const result = [];
+    for (let i=0; i < products.length; i++) {
+      let product = products[i];    
+      if (product.type == productType) {
+        result.push(product);
+      } 
+    } 
+    return result;
+  }
+  
+  let result1 = filterProducts(data, PRODUCT_TYPE.MILK_FOOD);
+  console.log(result1);
+  result1 = filterProducts(data, PRODUCT_TYPE.FRUIT);
+  console.log(result1);*/
+  
+  //let result2 = data.filter(function(product) {
+   // return product.type == PRODUCT_TYPE.MILK_FOOD;
+  //});
+  
+  //console.log(result2);
+  
+  //-Домашка 17----------------------------------------------
+  function Products(array) {
+    const products = array;
+    this.addProduct = function(product) {
+      products.push(product);
+    };
+    this.getProductsByType = function(productType) {
+      const result = [];
+      for (let i=0; i < products.length; i++) {
+        let product = products[i];    
+        if (product.type == productType) {
+          result.push(product);
+        } 
+      } 
+      return result;
+    };
+    this.getSum = function() {
+      let sum = 0;
+      for(let i=0; i<products.length; i++) {
+        let product = products[i];
+       sum+= product.quantity * product.price;
+      }
+      return sum;
+    };
+    this.getSumByType = function(productType) {    
+      const specificProducts = this.getProductsByType(productType);
+      let sum = 0;
+      for(let i=0; i<specificProducts.length; i++) {
+        let product = specificProducts[i];
+       sum+= product.quantity * product.price;
+      }
+      return sum;
+    };
+    };
+    let products1 = new Products(data);
+    console.log(products1.getSum());
+    console.log(products1.getSumByType(PRODUCT_TYPE.MILK_FOOD));
+    products1.addProduct({
+    name: 'Lemon',
+    price: 5,
+    quantity: 2,
+    type: PRODUCT_TYPE.FRUIT,
+   });
+   console.log(products1.getProductsByType(PRODUCT_TYPE.MILK_FOOD));
+   console.log(data);
